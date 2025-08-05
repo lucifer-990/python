e3d9b221627b38491711a82313aa816314c5f9d4
lucifer-990 <subhajitshaw16@gmail.com>
2025-08-05 20:14:52 +0530

    files added

--- Chapter 5/02_dict_methods.py
+++ Chapter 5/02_dict_methods.py
@@ -5,11 +5,11 @@ marks = {
     0: "Harry"
 }
 
-# print(marks.items())
-# print(marks.keys())
-# print(marks.values())
-# marks.update({"Harry": 99, "Renuka": 100})
-# print(marks)
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+marks.update({"Harry": 99, "Renuka": 100})
+print(marks)
 
 print(marks.get("Harry2")) # Prints None
 print(marks["Harry2"]) # Returns an error
